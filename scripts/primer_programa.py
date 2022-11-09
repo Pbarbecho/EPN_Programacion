a466bc36fbd2f46a48dbf7c12f9251bab40fec08
Pablo Barbecho <58553873+Pbarbecho@users.noreply.github.com>
2022-11-09 10:19:04 -0500

    Delete scripts directory

--- scripts/primer_programa.py
+++
@@ -1,5 +0,0 @@
-print("Hola Bienvenidos: Este es un script de python\n")
-
-print("1. Mis datos personales son: Pablo Barbecho")
-print("2. Vivo en: Cuenca - Ecuador")
-print("3. Imparto clases en: Primaria")
