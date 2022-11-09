88feee99d12fd709fbddd634e51cbf77000312fd
Pablo Barbecho <58553873+Pbarbecho@users.noreply.github.com>
2022-11-09 16:37:20 +0100

    Update primer_programa.py

--- ejercicios/primer_programa.py
+++ ejercicios/primer_programa.py
@@ -1,5 +1,4 @@
-print("Hola Bienvenidos: Este es un script de python\n")
-
-print("1. Mis datos personales son: Pablo Barbecho")
-print("2. Vivo en: Cuenca - Ecuador")
-print("3. Imparto clases en: Primaria")
+print("Hola Bienvenidos: Este es un script de python")
+print("1. Mi nombre es: Pablo Barbecho")
+print("2. Vivo en: Quito - Ecuador")
+print("3. Imparto clases en: EPN")
